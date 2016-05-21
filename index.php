8809194b153f646bf50d6e230228e7a3ed755cce
kasey <kmaberry@nmt.edu>
2016-05-20 19:43:44 -0600

    updated index

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
-	echo "Hello world.";
+	echo "Hello this is the first version of my website. I currently know very little in regards to web programming, however over the summer I plan on learning.\n";
 ?>
+
+The only working pages on this website are deploy.php which automatically pulls from git on my server whenever I push from my local machine. This script was obtained from https://github.com/markomarkovic/simple-php-git-deploy/
