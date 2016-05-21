d635c4cbb42eac9ae5478bef91e07d9b1e7ae74d
kasey <kmaberry@nmt.edu>
2016-05-21 10:27:55 -0600

    First design, needs improvments

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
-	echo "Hello this is the first version of my website. I currently know very little in regards to web programming, however over the summer I plan on learning.\n";
-?>
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+    <div id="top"><div>
+	  <ptitle>Kasey L<br>Maberry  </ptitle>
+    </div></div>
+     <div id="left"><div>
+     <div class="nav_link">About</div>
+     <div class="nav_link">Projects</div>
+     <div class="nav_link">Skills</div>
+     <div class="nav_link">Resume</div>
+     <div class="nav_link2">Contact</div>
+    </div></div>
+    <div id="right"><div>
 
-The only working pages on this website are deploy.php which automatically pulls from git on my server whenever I push from my local machine. This script was obtained from https://github.com/markomarkovic/simple-php-git-deploy/
+	The only working page on this website is deploy.php which automatically pulls from git on my server whenever I push from my local machine. This script was obtained from https://github.com/markomarkovic/simple-php-git-deploy/
+
+    </div></div>
+</body>
