bc237fb8468fabfd719b5dd31f35f091c9b4ce3e
kmaberry <me@me.com>
2017-09-18 23:16:46 -0600

    updated to use proper html and made it look better, matches resume

--- index.php
+++ index.php
@@ -1,42 +1,302 @@
+
+<!DOCTYPE html>
+<html lang="en">
 <head>
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<title> Kasey Maberry</title>
+  <title>Kasey Maberry</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
-    <div id="top"><div>
-	  <ptitle>Kasey L<br>Maberry  </ptitle>
-    </div></div>
-      <div id="left"><div>
-	  <?php require_once('dynam/navbar.php'); ?>
-      </div></div>
-    <div id="right"><div>
-    <h1> About Me</h1>
-	<p class="first"> My name is Kasey Maberry and I am currently a senior Computer Science major at 
-	New Mexico Tech with an expected graduation date of December 2017. I have been using computers daily
-	ever since I was six years old, it all started when my older brother introduced me to the video game
-	Diablo II developed by Blizzard North and published by Blizzard Entertainment in 2002. 
-	Shortly after that in either 2003 or 2004 I purchased a game known as StarCraft, a real time strategy game
-	developed and published
-	by Blizzard Entertainment. StarCraft is where my interest in Computer Science began, there are player made
-	maps that you can create and share with other players and this is where first encountered a very basic form of programming. The programming part was very simple, there was a list of premade "triggers", which were the instructions. Each
-	began with a conditional, and then if that was true then the action took place. For example, an extremely popular
-	trigger that I used in multiple maps was if a unit walked into a certain zone they would be
-	teleported to somewhere else, all of it was very simple and easy to make. I really enjoyed making maps, I would end
-	up spending more times creating a map then actually playing the game, and I made these maps for several years.
-	After that I knew I wanted to be a computer scientist, and in high school I took two introductory 
-	classes that I really enjoyed, one was for C++ and the other was for Python. I do not really remember much
-	from them. I started attending New Mexico Tech in Fall of 2013, and it will take me a total of 9 semesters to
-	graduate. I currently have a 3.31 GPA and have completed 6 semesters. As of right now I do not have a specific preference for what I want to
-	do once I graduate, however I find lower level programming such as C and Assembly to be incredibly interesting.
-	I also find security to be interesting. Reverse engineering is also something I find fun and something I wish to learn, in one course
-	we had to exploit programs that the professor gave us by reverse engineering and it was extremely fun and something I wish to learn more about.
-	 Back when I was making maps in
-	StarCraft I essentially learned everything by going through other maps and reading how they worked, with very
-	few tutorials on how it worked. Unfortunately, I do not have any of the maps I made for StarCraft as the
-	harddrive of that computer failed a couple of years ago after nearly 10 years of use.
-	 </p>
-	<!--<p>This is the first draft. This page will contain information about me and  The background was obtained from www.subtlepatterns.com. A script to have the server automatically pull from git was obtained from https://github.com/markomarkovic/simple-php-git-deploy/
-	</p>-->
 
-    </div></div>
+<div style="background-color: #ffffff" class="jumbotron text-center">
+  <h2><strong>Kasey Logan Maberry</strong></h2>
+  <!-- todo, fix colors to recommended way to do colors-->
+  <p><font color ="#dc3521">Computer Scientist </font><br >
+  	<font color="#999999">806 Liles Street, Socorro NM, 87801 </font><br>
+  	505-399-0131 | kmaberry@cs.nmt.edu | kmaberry.me | klogam@github
+  	</p> 
+
+</div>
+	
+	
+<div class="container">
+
+	<h4>Senior Computer science student looking for a career in either the Software Engineering or Security Field.</h4>
+	<hr>
+	<div class="row">
+		<div class="col-sm-12">
+			<h3><strong>Education</strong></h2>
+			<strong>New Mexico Institute of Mining and Technology</strong> <br />
+			<div class="redtext">B.S in Computer Science <br /></div>
+			3.3/4.0 Overall GPA <br />
+			3.5/4.0 Major GPA <br />
+			Graduating December 2017 <br />
+			<br />
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-12">
+			<h3><strong>Skills</strong></h3>
+			<table style ="width:100%">
+				<col width ="15%">
+				<col width ="85%">
+				<tr>
+					<td> <strong>Programming</strong> </td>
+					<td> C, Java, Bash Scripting, Python, PHP, LaTeX, Git, and Assembly (AT & T Syntax, 64 and 32 bit) </td>
+				</tr>		
+				<tr><td></td><td></td></tr>
+				<tr>
+					<td><strong>Web</strong></td>
+					<td>HTML5, CSS, PHP, MySQL, JavaScript, Java Servlets, SQL*PLUS, ER diagrams, relational models</td>
+				</tr>
+				<tr>
+					<td><strong>Network Security</strong></td>
+					<td>Hands-on experience with: Configuring ARP, NAT, IPSEC, DNSSEC, mitigating ARP spoofing
+and MITM attacks, DNS attacks, DDoS from TCP, Botnets, BGP Hijacking, cryptography</td>
+				</tr>
+				<tr>
+					<td><strong>Other Subjects</strong></td>
+					<td>Neural Networks, Networks, Algorithms and Data Structures, Computer Architecture, Automata,
+Principles of Programming Languages, and Software Engineering</td>
+				</tr>
+			</table>
+		</div>
+	</div>
+	<hr>
+	<!--
+	 * One instruction set
+	 -->
+	<div class="row">
+		<div class="col-sm-12">
+			<h3><strong>Projects</strong></h3>
+			<strong>Detecting a cheater in an online First Person Shooter using Neural Networks</strong><br />
+			<div class="redtext"><a href = "ann_fps_cheater.pdf">Download Research Paper here (Work in progress)</a></div>
+			<ul>
+				<li>Detected cheaters using LVQ and Multilayer Perceptron networks</li>
+				<li>Research conducting using a First Person Online Shooter, Counter-Strike: Global Offensive</li>
+				<li>Created 2400 manufactured data points in a controlled environment, finished project with 99% cheater detection and only false negatives </li>
+				<li>Used: Java, WEKA</li>
+			</ul>
+			<br/>
+			<strong>Detecting and removing a Botnet on Deterlab</strong><br />
+			<div class="redtext"><a href = "botnet.pdf">Download Research Paper here (Work in progress)</a></div>
+			<ul>	
+				<li>Used Deterlab to complete the project, and then wrote a writeup about what I did. Also looked into a popular opensource botnet to see how it worked</li>
+				<li>Looked through packets to find suspicious activities and possibly a way to alleviate a simulated computer network from a botnet that someone had installed on it</li>
+				<li>Looked into the Mirai botnet source code, to see how it worked</li>
+			</ul>
+			<br/>
+			<strong>Computer Aided Dispatch Website</strong><br />
+			<div class="redtext">Link to website currently unavailable</div>
+			<ul>
+				<li>Wrote a website to possibly be used as a computer aided dispatch center(For the dispatch to enter after someone calls 911)</li>
+				<li>Used PHP, CSS, and HTML, along with various software engineering design principles.</li>
+				<li>First web project, learned as I went</li>
+				<li>Used: HTML, CSS, PHP, MySQL</li>
+			</ul>
+
+			<strong>Vulcan Tech Forums</strong><br />
+			<div class="redtext"><a href = "https://weave.cs.nmt.edu/apollo.5/vulcanforums/">Click here to visit the website</a></div>
+			<ul>
+				<li>Wrote a mostly working forum website</li>
+				<li>The week before it was due it was found out that the host server does not support GET links, a lot of features did not get implemented due to having to rewrite all links (Forums do not work very well with POST)</li>
+				<li>Used: Java, Javascript, MySQL, and CSS</li>
+			</ul>
+			<br/>
+			<strong>Online shopping website</strong><br />
+			<div class="redtext"><a href = "http://infohost.nmt.edu/~kmaberry/project/">Click here to visit the website</a></div>
+			<ul>
+				<li>Wrote a prototype of a online shopping website for a course on Human Computer Interaction</li>
+				<li>Performed surveys on the website and collected what a user liked and did not like</li>
+				<li>Used: HTML, CSS, BootStrap</li>
+			</ul>
+
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-12">
+		<h3><strong>Experience</strong></h3>
+			<table style ="width:100%">
+				<col width ="85%">
+				<col width ="15%">
+				<tr>
+					<td><strong>New Mexico Tech Computer Science Department</strong></td>
+					<td style="text-align:right"><div class="redtext">Socorro, NM</div></td>
+				</tr>
+				<tr>
+					<td><div class="redtext">Grader</div></td>
+					<td style="text-align:right"><div class="redtext">January 2015-May 2015</div></td>
+				</tr>
+			</table>
+			<ul>
+			<li> Graded weekly student assignments, then met with the student to discuss grade, code quality, and to answer any questions regarding the course.</li>
+			<li> Graded for the Introduction to Computer Science course and Introduction to Data Structures and Algorithms.</li>
+			</ul>
+			<table style ="width:100%">
+				<col width ="85%">
+				<col width ="15%">
+				<tr>
+					<td><strong>New Mexico Tech Computer Science Department</strong></td>
+					<td style="text-align:right"><div class="redtext">Socorro, NM</div></td>
+				</tr>
+				<tr>
+					<td><div class="redtext">Tutor</div></td>
+					<td style="text-align:right"><div class="redtext">January 2016-May 2016</div></td>
+				</tr>
+			</table>
+			<ul>
+				<li>  Held weekly tutoring sessions for students to attend and receive help with the homework or ask questions.</li>
+				<li> Tutored for the Introduction to Object Oriented Programming course(Java).</li>
+			</ul>
+			<table style ="width:100%">
+				<col width ="85%">
+				<col width ="15%">
+				<tr>
+					<td><strong>Unrelated Experience</strong></td>
+					<td style="text-align:right"><div class="redtext">Estancia, NM</div></td>
+				</tr>
+				<tr>
+					<td><div class="redtext">Public works crewmember, YCC crewmember </div></td>
+					<td style="text-align:right"><div class="redtext">Summers of 2015-2017</div></td>
+				</tr>
+			</table>
+			<ul>
+			<li>Performed basic maintenance and landscaping for the Town of Estancia (Public Works, Summer 2015)</li>
+			<li> Installed water lines for the Town of Estancia Cemetary (YCC, Summer 2016)</li>
+			<li>Build a brick plaza for the Town of Estancia (YCC, Sumer 2017)</li>
+			</ul>
+
+			</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-12">
+			<h3><strong>Honors</strong></h3>
+			<h4>New Mexico Tech</h4>
+			<table style ="width:100%">
+				<col width ="20%">
+				<col width ="60%">
+				<col width ="20%">
+				<tr>
+					<td><div class="graytext">2013</div></td>
+					<td><div class="graytext">Presidential Scholarship</div></td>
+					<td style="text-align:right"><div class="redtext">Socorro, NM </div></td>
+				</tr>
+				<tr>
+					<td><div class="graytext">2014-Present</div></td>
+					<td><div class="graytext">Honor Roll</div></td>
+					<td style="text-align:right"><div class="redtext">Socorro, NM </div></td>
+				</tr>
+			</table>
+		</div>
+	</div>	
+	<!--
+	 * End instruction set
+	 -->
+	
+	<!--
+	 * One instruction set
+	 -->
+	 <hr>
+	<div class="row">
+		<div class="col-sm-12">
+			<h4>Other</h4>
+			<a href = "kmaberry_resume.pdf">A current link to my resume</a>
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="iholder customer"></div>
+		</div>
+		
+		<div class="col-sm-6">
+			<p>Interact with the customer portion of the website (15-20 Minutes)<br />
+			<a href = "customer.php" target="_blank">Go to customer portion</a></p>
+		</div>
+	</div>
+	<!--
+	 * End instruction set
+	 -->
+	
+	<!--
+	 * One instruction set
+	 -->
+	<div class="row">
+		<div class="col-sm-12">
+			<h4>Step 3</h4>
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="iholder employee"></div>
+		</div>
+		
+		<div class="col-sm-6">
+			<p>Interact with the employee portion of the website (15-20 Minutes)<br />
+			<a href = "employee/employee.php" target="_blank">Go to employee portion</a></p>
+		</div>
+	</div>
+	<!--
+	 * End instruction set
+	 -->
+	
+	<!--
+	 * One instruction set
+	 -->
+	<div class="row">
+		<div class="col-sm-12">
+			<h4>Step 4</h4>
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="iholder manager"></div>
+		</div>
+		
+		<div class="col-sm-6">
+			<p>Interact with the store manager portion of the website (15-20 Minutes)<br />
+			<a href = "manager/manager.php" target="_blank">Go to manager portion</a></p>
+		</div>
+	</div>
+	<!--
+	 * End instruction set
+	 -->
+	
+	<!--
+	 * One instruction set
+	 -->
+	<div class="row">
+		<div class="col-sm-12">
+			<h4>Step 5</h4>
+		</div>
+	</div>
+	<hr>
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="iholder survey2"></div>
+		</div>
+		
+		<div class="col-sm-6">
+			Please take the exit survey (Fill out questions regarding our website)<br>
+			<a href="https://goo.gl/forms/ngHW1YZFhpNIeRIm1" target="_blank">Link to survey</a>
+		</div>
+	</div>
+	<!--
+	 * End instruction set
+	 -->
+	
+	<hr>
+	
+	
+	</div>
 </body>
+</html>
